f0df2fdc56f3b5dfdff4391001dddde38240178c
Jeff Thieleke <github@thieleke.com>
2026-03-11 14:42:41 -0500

    Remove duplicate function from the branch mergers

--- js/tools/textTools.js
+++ js/tools/textTools.js
@@ -125,16 +125,6 @@ class TextTools { /* exported TextTools*/
     return textCharCodeArray;
   }
 
-  static escapeHtml(text) {
-    if (!text) { return ''; }
-    return String(text)
-      .replace(/&/g, '&amp;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#39;');
-  }
-
   static isStringUrl(textUrl) {
     let isValidUrl = true;
     try { new URL(textUrl); }
